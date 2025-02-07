6faa41fa025db1962668668339e27077d2bcbcc9
Dautkhan <bbdavi@icloud.com>
2025-02-07 14:51:34 +0500

    prime_numbers

---
+++ lab3/6.py
@@ -0,0 +1,12 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+numbers = [2, 3, 4, 5, 10, 11, 12, 13, 14, 15]
+
+prime_numbers = list(filter(lambda x: is_prime(x), numbers))
+print(f"Prime numbers: {prime_numbers}")
